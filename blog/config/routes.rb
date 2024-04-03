ffe52f84d378eb5f614a814ea89bc05f077bda66
ablaspinto <ablaspinto@gmail.com>
2024-04-02 23:57:29 -0400

    adding create post operation

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get "/posts/:id" , to: "posts#show" 
-  post 
-  put 
-  patch 
-  delete 
+  get "/post/new" , to: "posts#new" , as: :new_post
+
+  get "/posts/:id" , to: "posts#show" , as: :post
+
+
 
   # Defines the root path route ("/")
   root "posts#index"
